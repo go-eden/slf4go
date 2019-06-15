e4c2ad041046d499d687f9298c3d2a503ac17793
sulin <sulinixl@163.com>
2019-06-15 22:20:30 +0800

    performance optimize

--- xlog_logger.go
+++ xlog_logger.go
@@ -23,102 +23,156 @@ func (l *Logger) GetName() string {
 }
 
 func (l *Logger) IsEnableTrace() bool {
-	pc, _, _, _ := runtime.Caller(1)
+	var pc uintptr
+	if l.name != "" {
+		pc, _, _, _ = runtime.Caller(1)
+	}
 	return l.level(pc) >= LEVEL_TRACE
 }
 
 func (l *Logger) IsEnableDebug() bool {
-	pc, _, _, _ := runtime.Caller(1)
+	var pc uintptr
+	if l.name != "" {
+		pc, _, _, _ = runtime.Caller(1)
+	}
 	return l.level(pc) >= LEVEL_DEBUG
 }
 
 func (l *Logger) IsEnableInfo() bool {
-	pc, _, _, _ := runtime.Caller(1)
+	var pc uintptr
+	if l.name != "" {
+		pc, _, _, _ = runtime.Caller(1)
+	}
 	return l.level(pc) >= LEVEL_INFO
 }
 
 func (l *Logger) IsEnableWarn() bool {
-	pc, _, _, _ := runtime.Caller(1)
+	var pc uintptr
+	if l.name != "" {
+		pc, _, _, _ = runtime.Caller(1)
+	}
 	return l.level(pc) >= LEVEL_WARN
 }
 
 func (l *Logger) IsEnableError() bool {
-	pc, _, _, _ := runtime.Caller(1)
+	var pc uintptr
+	if l.name != "" {
+		pc, _, _, _ = runtime.Caller(1)
+	}
 	return l.level(pc) >= LEVEL_ERROR
 }
 
 func (l *Logger) IsEnableFatal() bool {
-	pc, _, _, _ := runtime.Caller(1)
+	var pc uintptr
+	if l.name != "" {
+		pc, _, _, _ = runtime.Caller(1)
+	}
 	return l.level(pc) >= LEVEL_FATAL
 }
 
 func (l *Logger) Trace(v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_TRACE {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprint(v...)
 	l.print(NewLog(LEVEL_TRACE, pc, fileName, line, msg))
 }
 
 func (l *Logger) Tracef(format string, v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_TRACE {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprintf(format, v...)
 	l.print(NewLog(LEVEL_TRACE, pc, fileName, line, msg))
 }
 
 func (l *Logger) Debug(v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_DEBUG {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprint(v...)
 	l.print(NewLog(LEVEL_DEBUG, pc, fileName, line, msg))
 }
 
 func (l *Logger) Debugf(format string, v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_DEBUG {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprintf(format, v...)
 	l.print(NewLog(LEVEL_DEBUG, pc, fileName, line, msg))
 }
 
 func (l *Logger) Info(v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_INFO {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprint(v...)
 	l.print(NewLog(LEVEL_INFO, pc, fileName, line, msg))
 }
 
 func (l *Logger) Infof(format string, v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_INFO {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprintf(format, v...)
 	l.print(NewLog(LEVEL_INFO, pc, fileName, line, msg))
 }
 
 func (l *Logger) Warn(v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_WARN {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprint(v...)
 	l.print(NewLog(LEVEL_WARN, pc, fileName, line, msg))
 }
 
 func (l *Logger) Warnf(format string, v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_WARN {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprintf(format, v...)
 	l.print(NewLog(LEVEL_WARN, pc, fileName, line, msg))
 }
 
 func (l *Logger) Error(v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_ERROR {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprint(v...)
 	l.print(NewLog(LEVEL_ERROR, pc, fileName, line, msg))
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_ERROR {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprintf(format, v...)
 	l.print(NewLog(LEVEL_ERROR, pc, fileName, line, msg))
 }
 
 func (l *Logger) Fatal(v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_FATAL {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprint(v...)
 	l.print(NewLog(LEVEL_FATAL, pc, fileName, line, msg))
 }
 
 func (l *Logger) Fatalf(format string, v ...interface{}) {
+	if l.name != "" && l.level(0) > LEVEL_FATAL {
+		return // don't need log
+	}
 	pc, fileName, line, _ := runtime.Caller(1)
 	msg := fmt.Sprintf(format, v...)
 	l.print(NewLog(LEVEL_FATAL, pc, fileName, line, msg))
